7b36bc23012493c8d4815caa8fca73c6fabc23cb
innabahamya <i.kurashevich@gmail.com>
2023-09-21 10:36:33 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const sum = document.querySelector('.sum').value;
+const button = document.querySelector('.button');
+const total = document.querySelector('.total');
+
+let interestRate = 18.5;
+
+const deposit = '30000';
+
+const growth = '150';
+
+const currency = 'руб';
+
+console.log( 'Ваш депозит на начало расчётного периода составлял ' +  deposit + ' ' + currency + '.' );
+console.log( 'Согласно вашему тарифу, вам была присвоена ставка ' + interestRate + '%' );
+
+
+let depositAsNumber = Number(deposit); 
+// console.log(depositAsNumber); 
+
+let growthAsNumber = Number(growth); 
+// console.log(growthAsNumber); 
+let newSum = depositAsNumber + growthAsNumber;
+
+console.log('К концу расчётного периода прирост составил ' + growth + ' ' + currency + '.,' + ' и на данный момент ваш депозит составляет ' + newSum +  currency + ' .' );
+
+button.addEventListener('click', () => {
+  const sum = document.querySelector('.sum').value;
+  total.textContent = (message); // Через год у вас будет XXX руб. на счету", где вместо ХХХ выведите сколько получится на счету с учетом процентной ставки 7% годовых
+});
